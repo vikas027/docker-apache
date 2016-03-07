beae4a99a5b187d0e117b12b6b50f39978ab7bdc
Vikas Kumar <vikas027@users.noreply.github.com>
2016-03-07 16:46:37 +1100

    Added Timezone Environment Variable

--- start-httpd.sh
+++ start-httpd.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+if [ ! -z "${TIMEZONE}" ]
+then
+   rm -f /etc/localtime
+   ln -sf /usr/share/zoneinfo/"${TIMEZONE}" /etc/localtime
+fi
+
 rm -rf /run/httpd/* /tmp/httpd*
 exec /usr/sbin/apachectl -D FOREGROUND
